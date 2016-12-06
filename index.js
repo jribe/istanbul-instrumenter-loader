aa681929cd85a66bf3e9d4b46fce25848fab5191
James Ribe <jribe@tableau.com>
2016-12-06 12:37:08 -0800

    added the 'noInputSourceMap' option

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
 var istanbulLibInstrument = require('istanbul-lib-instrument');
 var loaderUtils = require('loader-utils');
 var assign = require('object-assign');
+var sourceMap = require('source-map');
 
-module.exports = function(source, sourceMap) {
+function concatenateSourceMaps(sourceMaps) {
+  var firstMap = new sourceMap.SourceMapConsumer(sourceMaps[0]);
+  var generator = sourceMap.SourceMapGenerator.fromSourceMap(firstMap);
+  for (var i = 1; i < sourceMaps.length; i++) {
+    var nextMap = new sourceMap.SourceMapConsumer(sourceMaps[i]);
+    generator.applySourceMap(nextMap);
+  }
+  return generator.toJSON();
+}
+
+module.exports = function(source, inputSourceMap) {
     var userOptions = loaderUtils.parseQuery(this.query);
     var instrumenter = istanbulLibInstrument.createInstrumenter(
         assign({ produceSourceMap: this.sourceMap }, userOptions)
@@ -14,8 +25,15 @@ module.exports = function(source, sourceMap) {
         this.cacheable();
     }
 
+    var instrumenterSourceMap = userOptions.noInputSourceMap ? undefined : inputSourceMap;
     var that = this;
     return instrumenter.instrument(source, this.resourcePath, function (error, source) {
-        that.callback(error, source, instrumenter.lastSourceMap());
-    }, sourceMap);
+        var outputSourceMap;
+        if (userOptions.noInputSourceMap && this.sourceMap) {
+          outputSourceMap = concatenateSourceMaps([instrumenter.lastSourceMap(), inputSourceMap]);
+        } else {
+          outputSourceMap = instrumenter.lastSourceMap();
+        }
+        that.callback(error, source, outputSourceMap);
+    }, instrumenterSourceMap);
 };
